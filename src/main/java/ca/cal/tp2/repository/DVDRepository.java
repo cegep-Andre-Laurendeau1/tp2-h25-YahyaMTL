e4abc7b28cf56f7b9587879eac0bbbf383960645
YahyaMTL <yahya.osman17@hotmail.com>
2025-03-11 02:48:32 -0400

    Implémentation des fonctionnalités de recherche pour les DVD.

--- src/main/java/ca/cal/tp2/repository/DVDRepository.java
+++ src/main/java/ca/cal/tp2/repository/DVDRepository.java
@@ -1,4 +1,13 @@
 package ca.cal.tp2.repository;
 
+import ca.cal.tp2.modele.DVD;
+
+import java.util.List;
+
 public interface DVDRepository {
+    void save (DVD dvd);
+    DVD findById(long id);
+    List<DVD> findByYear(int annee);
+    DVD findByTitle(String titre);
+    DVD findByDirector(String directeur);
 }
--- src/main/java/ca/cal/tp2/repository/DVDRepositoryJPA.java
+++ src/main/java/ca/cal/tp2/repository/DVDRepositoryJPA.java
@@ -1,4 +1,59 @@
 package ca.cal.tp2.repository;
 
-public class DVDRepositoryJPA {
+import ca.cal.tp2.modele.DVD;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class DVDRepositoryJPA implements DVDRepository {
+    private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("tp2");
+
+    public void save(DVD dvd) {
+        try {
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            em.persist(dvd);
+            em.getTransaction().commit();
+            em.close();
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public DVD findById(long id) {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<DVD> query = em.createNamedQuery("SELECT id FORM DVD" + "WHERE id = :id", DVD.class);
+        query.setParameter("id", id);
+        return query.getSingleResult();
+    }
+
+    public List<DVD> findByYear (int annee) {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<DVD> query = em.createNamedQuery("SELECT datePublication FROM DVD"
+                + "WHERE datePublication BETWEEN :anneeDebut AND :anneeFin", DVD.class);
+        query.setParameter("anneeDebut", LocalDate.of(annee, 1, 1));
+        query.setParameter("anneeFin", LocalDate.of(annee, 12, 31));
+        return query.getResultList();
+    }
+
+    public DVD findByDirector (String director) {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<DVD> query = em.createNamedQuery("SELECT directeur FROM DVD" +
+                " WHERE directeur.directeur = :director", DVD.class);
+        query.setParameter("director", director);
+        return query.getResultList().get(0);
+    }
+
+    public DVD findByTitle (String titre) {
+        EntityManager em = emf.createEntityManager();
+        TypedQuery<DVD> query = em.createNamedQuery("SELECT titre FROM DVD" +
+                " WHERE titre = :titre", DVD.class);
+        query.setParameter("titre", titre);
+        return query.getResultList().get(0);
+    }
 }
